a7bce98e2db822695be4f71543886d6ff22604db
lukas <lukede543@gmail.com>
2025-12-09 14:29:30 +0100

    Implement Review representation in domain and api layer
    - implement Review model in domain layer
    - implement ReviewDto in api layer

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -2,14 +2,32 @@ package de.seuhd.campuscoffee.api.dtos;
 
 import lombok.Builder;
 import org.jspecify.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 /**
  * DTO record for POS metadata.
  */
 @Builder(toBuilder = true)
 public record ReviewDto (
-    @Nullable Long id
+    @Nullable Long id,
     // TODO: Implement ReviewDto
+    @Nullable LocalDateTime createdAt,
+    @Nullable LocalDateTime updatedAt,
+
+    @NotNull
+    @NonNull Long posID,
+
+    @NotNull
+    @NonNull Long authorID,
+
+    @NotBlank(message = "Review description cannot be empty.")
+    @NonNull String reviewDesc,
+
+    @NotNull
+    @NonNull Boolean approved
 ) implements Dto<Long> {
     @Override
     public @Nullable Long getId() {
--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/objects/Review.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/objects/Review.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
+import java.time.LocalDateTime;
+
 /**
  * Domain record that stores a review for a point of sale.
  * Reviews are approved once they received a configurable number of approvals.
@@ -12,6 +14,11 @@ import org.jspecify.annotations.Nullable;
 public record Review(
         @Nullable Long id, // null when the review has not been created yet
         //TODO: Implement review domain model.
+        @Nullable LocalDateTime createdAt,
+        @Nullable LocalDateTime updatedAt,
+        @NonNull Long posID,
+        @NonNull Long authorID,
+        @NonNull String reviewDesc,
         @NonNull Integer approvalCount, // is updated by the domain module
         @NonNull Boolean approved // is determined by the domain module
 ) implements DomainModel<Long> {
